d8c91119da8c1f28c52663c6f9681e2cc81595b8
Victor Pérez Ngounou <71229551+vngounou26@users.noreply.github.com>
2024-02-09 14:18:03 +0100

    fix: Insecure Direct Object Reference (IDOR)

--- juice-shop-master/routes/basket.ts
+++ juice-shop-master/routes/basket.ts
@@ -15,7 +15,8 @@ const challenges = require('../data/datacache').challenges
 module.exports = function retrieveBasket () {
   return (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id
-    BasketModel.findOne({ where: { id }, include: [{ model: ProductModel, paranoid: false, as: 'Products' }] })
+    const userId = req.user.id // recupération de l'id de l'utilisateur connecté
+    BasketModel.findOne({ where: { id, userId }, include: [{ model: ProductModel, paranoid: false, as: 'Products' }] })
       .then((basket: BasketModel | null) => {
         /* jshint eqeqeq:false */
         challengeUtils.solveIf(challenges.basketAccessChallenge, () => {
